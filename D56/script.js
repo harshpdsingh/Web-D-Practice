25fbb88ecd2f5eeb5ccf545aa2c117061106fc4d
Harsh Pd Singh <harshpdsingh@gmail.com>
2025-03-21 01:56:24 +0530

    Practice

---
+++ D56/script.js
@@ -0,0 +1,14 @@
+let age = 3;
+let grace = 3
+if(age>18){
+    console.log("You can drive");
+}
+else{
+    console.log("You can't drive");
+}
+
+console.log(age+grace)
+console.log(age-grace)
+console.log(age*grace)
+console.log(age/grace)
+console.log(age**grace)
