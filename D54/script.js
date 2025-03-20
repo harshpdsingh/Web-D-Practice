92f1ab74753eea8b2180130ee3470eccf2c9b91c
Harsh Pd Singh <harshpdsingh@gmail.com>
2025-03-21 00:57:57 +0530

    Practice

---
+++ D54/script.js
@@ -0,0 +1,7 @@
+// alert("Hello World");
+
+console.log("Code is running...");
+
+document.title = 123;
+
+document.body.style.backgroundColor = "Grey";
---
+++ D55/script.js
@@ -0,0 +1,7 @@
+console.log("Hey I am here...");
+
+var a = 9;
+var b = 10;
+var c = "HI";
+console.log(a+b);
+console.log(typeof c);
